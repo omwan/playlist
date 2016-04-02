ac72eb22f5b590852282805cd5d53143f42d89ee
Olivia Wan <oliviawan97@gmail.com>
2016-04-02 18:05:37 -0400

    so many

--- spotifyThing.py
+++ spotifyThing.py
@@ -6,19 +6,21 @@ SPOTIPY_CLIENT_SECRET='b3b19f4192804965b6529bb9cb1c6509'
 SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
 scope = 'user-library-read'
 
-if len(sys.argv)>1:
-    username = sys.argv[1]
-else:
-    print "Usage: %s username" % (sys.argv[0],)
-    sys.exit()
 
-token = util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
+def func(username):
+    if len(sys.argv)>1:
+        username = sys.argv[1]
+    else:
+        print "Usage: %s username" % (sys.argv[0],)
+        sys.exit()
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
-else:
-    print "Can't get token for", username
+    token = util.prompt_for_user_token(username, scope, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_saved_tracks()
+        for item in results['items']:
+            track = item['track']
+            print track['name'] + ' - ' + track['artists'][0]['name']
+    else:
+        print "Can't get token for", username
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from test2 import func
 app = Flask(__name__)
 
 from flask import render_template
@@ -7,8 +8,10 @@ from flask import render_template
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
+
+@app.route('/test/')
+def test(name=None):
+    return func()
         
-
-
 if __name__ == "__main__":
     app.run()
