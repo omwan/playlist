44c9438dab264da1e08877f4194bf352248b2fba
Olivia Wan <oliviawan97@gmail.com>
2016-04-02 23:40:52 -0400

    adding scope

--- spotifyThing.py
+++ spotifyThing.py
@@ -3,7 +3,6 @@ import spotipy
 import spotipy.util as util
 import os
 from spotipy.oauth2 import SpotifyOAuth
-scope = 'user-library-read'
 
 
 def authorize():
@@ -12,5 +11,6 @@ def authorize():
     client_id = os.getenv('SPOTIPY_CLIENT_ID')
     client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
     redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
+    scope = 'user-library-read'
 
-    return SpotifyOAuth(client_id, client_secret, redirect_uri)
+    return SpotifyOAuth(client_id, client_secret, redirect_uri, scope)
