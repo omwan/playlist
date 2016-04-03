8273e9d5ebb0330071d2f78ce96f9cd835c03029
Olivia Wan <oliviawan97@gmail.com>
2016-04-03 01:34:40 -0400

    removed print statements from showPlaylist.py

--- showPlaylists.py
+++ showPlaylists.py
@@ -10,23 +10,24 @@ def show_tracks(token):
     sp = spotipy.Spotify(auth=token)
     username = sp.me()["id"]
     playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
-        if playlist['owner']['id'] == username:
-            print
-            print playlist['name']
-            print '  total tracks', playlist['tracks']['total']
-            results = sp.user_playlist(username, playlist['id'],
-                fields="tracks,next")
-            tracks = results['tracks']
-            show_tracks(tracks)
-            while tracks['next']:
-                tracks = sp.next(tracks)
-                show_tracks(tracks)
-                
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        return "   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name'])
+    return playlists
+##    for playlist in playlists['items']:
+##        if playlist['owner']['id'] == username:
+##            print
+##            print playlist['name']
+##            print '  total tracks', playlist['tracks']['total']
+##            results = sp.user_playlist(username, playlist['id'],
+##                fields="tracks,next")
+##            tracks = results['tracks']
+##            show_tracks(tracks)
+##            while tracks['next']:
+##                tracks = sp.next(tracks)
+##                show_tracks(tracks)
+##                
+##    for i, item in enumerate(tracks['items']):
+##        track = item['track']
+##        return "   %d %32.32s %s" % (i, track['artists'][0]['name'],
+##            track['name'])
 
 
     
