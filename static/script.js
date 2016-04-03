78b03c109754b93586c7e1149797e61038c28774
Olivia Wan <oliviawan97@gmail.com>
2016-04-02 21:04:54 -0400

    running test

--- static/script.js
+++ static/script.js
@@ -16,6 +16,7 @@ function embed() {
 }
 
 $(document).ready(function(){
+    console.log("running")
     //get the json generated from the website, and do this function to it
     $.get('https://api.spotify.com/v1/me/playlists?limit=10&offset=5', function(data){
         console.log(JSON.parse(data.text).items)
