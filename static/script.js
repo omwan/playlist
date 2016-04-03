ef8bfb543867a3a0663c7f844c35e34cc781a780
Olivia Wan <oliviawan97@gmail.com>
2016-04-03 02:31:01 -0400

    changing script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,8 @@
 var $SCRIPT_ROOT = {{ request.app|tojson|safe }};
 
-$(function( {
-    $.getJSON($SCRIPT_ROOT + "/tracks", function(playlists) {
-        for (var i = 0; i < playlists.length; i++) {
-            $(".playlists").append($('<li></li>').html(playlists[i].name))
-        }
-    })
-}))
+$(function() {
+    $.getJSON($SCRIPT_ROOT + '/tracks', function(data) {
+        $(".playlists").text(data.result);
+    });
+    return false;
+});
