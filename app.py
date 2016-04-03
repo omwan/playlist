3e5874584beb0dee8f54d3722386be06c22ea24c
Olivia Wan <oliviawan97@gmail.com>
2016-04-02 23:03:58 -0400

    testing token

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
-from flask import Flask
+from flask import Flask, session
 from spotifyThing import authorize
-from showPlaylists import getToken
+from showPlaylists import show_tracks
 from flask import redirect
 from flask import request
 app = Flask(__name__)
+SESSION TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 from flask import render_template
 
 @app.route('/')
 def hello(name=None):
-    return render_template('index.html', name=name)
+    token = request.args.get("code", None)
+    if token is None:
+        return render_template('index.html', name=name)
+    else:
+        sp = spotipy.Spotify(auth=token)
+        user = sp.me()
+        session['token'] = token
+        print "we have a token, which is: " + token
+        return render_template('index.html', name=name)
 
 @app.route('/auth/')
-def test(name=None):
+def test(name=None)
     return redirect(authorize(), code=302)
 
 @app.route('/tracks')
 def tracks(name=None):
-    return getToken();
+    show_tracks(session.get('token', None))
+    return render_template('index.html', name=name)
         
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
--- showPlaylists.py
+++ showPlaylists.py
@@ -5,65 +5,28 @@ import spotipy
 import spotipy.util as util
 from spotifyThing import authorize
 
-##def show_tracks(request):
-##    for i, item in enumerate(tracks['items']):
-##        track = item['track']
-##        return "   %d %32.32s %s" % (i, track['artists'][0]['name'],
-##            track['name'])
-##
-##
-##if __name__ == '__main__':
-##    if len(sys.argv) > 1:
-##        username = sys.argv[1]
-##    else:
-##        print "Whoops, need your username!"
-##        print "usage: python user_playlists.py [username]"
-##        sys.exit()
-##
-##    token = util.prompt_for_user_token(username)
-##
-##    if token:
-##        sp = spotipy.Spotify(auth=token)
-##        playlists = sp.user_playlists(username)
-##        for playlist in playlists['items']:
-##            if playlist['owner']['id'] == username:
-##                print
-##                print playlist['name']
-##                print '  total tracks', playlist['tracks']['total']
-##                results = sp.user_playlist(username, playlist['id'],
-##                    fields="tracks,next")
-##                tracks = results['tracks']
-##                show_tracks(tracks)
-##                while tracks['next']:
-##                    tracks = sp.next(tracks)
-##                    show_tracks(tracks)
-##    else:
-##        print "Can't get token for", username
-##
-
-
-
-def getToken():
-
-##    client_id = os.getenv('SPOTIPY_CLIENT_ID')
-##    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-##    redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
-##
-##    sp_oauth = SpotifyOAuth(client_id, client_secret, redirect_uri)
-##    
-##    auth_url = authorize()
-##    code = sp_oauth.parse_response_code(auth_url)
-##    token_info = sp_oauth.get_access_token(code)
+def show_tracks(token):
     
-    scope = 'user-library-read'
-    token = util.prompt_for_user_token(username, scope)
-
-    returnString = ""
-
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
+    username = sp.me()["id"]
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:
+        if playlist['owner']['id'] == username:
+            print
+            print playlist['name']
+            print '  total tracks', playlist['tracks']['total']
+            results = sp.user_playlist(username, playlist['id'],
+                fields="tracks,next")
+            tracks = results['tracks']
+            show_tracks(tracks)
+            while tracks['next']:
+                tracks = sp.next(tracks)
+                show_tracks(tracks)
+                
+    for i, item in enumerate(tracks['items']):
         track = item['track']
-        returnString = returnString + track['name'] + ' - ' + track['artists'][0]['name']
+        return "   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name'])
 
-    return returnString
+
+    
